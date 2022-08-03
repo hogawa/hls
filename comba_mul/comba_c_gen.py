ba41ba113e67a9228c2bd1463565eda85ce093aa
Henrique Ogawa <ogawa.henrique@gmail.com>
2022-08-03 18:56:10 -0300

    fix(comba): fixed minor C macro definitions

--- comba_mul/comba_c_gen.py
+++ comba_mul/comba_c_gen.py
@@ -46,13 +46,13 @@ def comba_c_gen(varlist_a: List, varlist_b: List, varlist_out: List):
     exp_buffer += ("\t__uint128_t sum = 0;\n")
     exp_buffer += ("\n")
     exp_buffer += ("\t" + varlist_out[0] + " = acc0;\n")
-    exp_buffer += ("\t" + varlist_out[1] + " = L(acc1);\n")
+    exp_buffer += ("\t" + varlist_out[1] + " = CMB_L64(acc1);\n")
     exp_buffer += ("\n")
     for i in range(2, len(varlist_out)):
-        exp_buffer += ("\tsum = HIGH64(acc" + str(i - 1) + ") + LOW64(acc" + str(i) + ") + carry;\n")
-        exp_buffer += ("\t" + varlist_out[i] + " = LOW64(sum);\n")
+        exp_buffer += ("\tsum = CMB_H64(acc" + str(i - 1) + ") + CMB_L64(acc" + str(i) + ") + carry;\n")
+        exp_buffer += ("\t" + varlist_out[i] + " = CMB_L64(sum);\n")
         if i < len(varlist_out) - 1:
-            exp_buffer += ("\tcarry = HIGH64(sum);\n")
+            exp_buffer += ("\tcarry = CMB_H64(sum);\n")
             exp_buffer += ("\n")
 
     return exp_buffer
@@ -63,13 +63,15 @@ if __name__ == "__main__":
     func_name = "comba256x256"
     vlist_a = ["a[0]", "a[1]", "a[2]", "a[3]"]
     vlist_b = ["b[0]", "b[1]", "b[2]", "b[3]"]
-    vlist_out = ["out0", "out1", "out2", "out3", "out4", "out5", "out6", "out7"]
+    vlist_out = ["out[0]", "out[1]", "out[2]", "out[3]", "out[4]", "out[5]", "out[6]", "out[7]"]
 
     if mode == "standalone":
         print("#include <stdint.h>")
         print("")
         print("#define CMB_MULL(X,Y) (X * Y)")
         print("#define CMB_MULH(X,Y) (((__uint128_t)X * Y) >> 64)")
+        print("#define CMB_L64(X)    ((uint64_t)X)")
+        print("#define CMB_H64(X)    (X >> 64)")
         print("")
         print("void " + func_name + "(uint64_t *out, uint64_t *a, uint64_t *b) {")
     
