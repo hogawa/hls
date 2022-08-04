8165e08aa5abf1a532bf91af8420f2c9b87dbfd8
Henrique Ogawa <ogawa.henrique@gmail.com>
2022-08-04 17:11:30 -0300

    feat(comba): adding more auto-generated samples

---
+++ comba_mul/tests_c/comba512x512.c
@@ -0,0 +1,90 @@
+/**
+ * Auto-generated with comba_c_gen.py
+ */
+#include <stdint.h>
+
+#define CMB_MULL(X,Y) (__uint128_t)(X * Y)
+#define CMB_MULH(X,Y) (((__uint128_t)X * (__uint128_t)Y) >> 64)
+#define CMB_L64(X)    (X & 0xFFFFFFFFFFFFFFFF)
+#define CMB_H64(X)    (X >> 64)
+
+void comba512x512(uint64_t *out, uint64_t *a, uint64_t *b) {
+	__uint128_t acc0 = CMB_MULL(a[0], b[0]);
+	__uint128_t acc1 = CMB_MULL(a[1], b[0]) + CMB_MULL(a[0], b[1]) + CMB_MULH(a[0], b[0]);
+	__uint128_t acc2 = CMB_MULL(a[2], b[0]) + CMB_MULL(a[1], b[1]) + CMB_MULL(a[0], b[2]) + CMB_MULH(a[1], b[0]) + CMB_MULH(a[0], b[1]);
+	__uint128_t acc3 = CMB_MULL(a[3], b[0]) + CMB_MULL(a[2], b[1]) + CMB_MULL(a[1], b[2]) + CMB_MULL(a[0], b[3]) + CMB_MULH(a[2], b[0]) + CMB_MULH(a[1], b[1]) + CMB_MULH(a[0], b[2]);
+	__uint128_t acc4 = CMB_MULL(a[4], b[0]) + CMB_MULL(a[3], b[1]) + CMB_MULL(a[2], b[2]) + CMB_MULL(a[1], b[3]) + CMB_MULL(a[0], b[4]) + CMB_MULH(a[3], b[0]) + CMB_MULH(a[2], b[1]) + CMB_MULH(a[1], b[2]) + CMB_MULH(a[0], b[3]);
+	__uint128_t acc5 = CMB_MULL(a[5], b[0]) + CMB_MULL(a[4], b[1]) + CMB_MULL(a[3], b[2]) + CMB_MULL(a[2], b[3]) + CMB_MULL(a[1], b[4]) + CMB_MULL(a[0], b[5]) + CMB_MULH(a[4], b[0]) + CMB_MULH(a[3], b[1]) + CMB_MULH(a[2], b[2]) + CMB_MULH(a[1], b[3]) + CMB_MULH(a[0], b[4]);
+	__uint128_t acc6 = CMB_MULL(a[6], b[0]) + CMB_MULL(a[5], b[1]) + CMB_MULL(a[4], b[2]) + CMB_MULL(a[3], b[3]) + CMB_MULL(a[2], b[4]) + CMB_MULL(a[1], b[5]) + CMB_MULL(a[0], b[6]) + CMB_MULH(a[5], b[0]) + CMB_MULH(a[4], b[1]) + CMB_MULH(a[3], b[2]) + CMB_MULH(a[2], b[3]) + CMB_MULH(a[1], b[4]) + CMB_MULH(a[0], b[5]);
+	__uint128_t acc7 = CMB_MULL(a[7], b[0]) + CMB_MULL(a[6], b[1]) + CMB_MULL(a[5], b[2]) + CMB_MULL(a[4], b[3]) + CMB_MULL(a[3], b[4]) + CMB_MULL(a[2], b[5]) + CMB_MULL(a[1], b[6]) + CMB_MULL(a[0], b[7]) + CMB_MULH(a[6], b[0]) + CMB_MULH(a[5], b[1]) + CMB_MULH(a[4], b[2]) + CMB_MULH(a[3], b[3]) + CMB_MULH(a[2], b[4]) + CMB_MULH(a[1], b[5]) + CMB_MULH(a[0], b[6]);
+	__uint128_t acc8 = CMB_MULL(a[7], b[1]) + CMB_MULL(a[6], b[2]) + CMB_MULL(a[5], b[3]) + CMB_MULL(a[4], b[4]) + CMB_MULL(a[3], b[5]) + CMB_MULL(a[2], b[6]) + CMB_MULL(a[1], b[7]) + CMB_MULH(a[7], b[0]) + CMB_MULH(a[6], b[1]) + CMB_MULH(a[5], b[2]) + CMB_MULH(a[4], b[3]) + CMB_MULH(a[3], b[4]) + CMB_MULH(a[2], b[5]) + CMB_MULH(a[1], b[6]) + CMB_MULH(a[0], b[7]);
+	__uint128_t acc9 = CMB_MULL(a[7], b[2]) + CMB_MULL(a[6], b[3]) + CMB_MULL(a[5], b[4]) + CMB_MULL(a[4], b[5]) + CMB_MULL(a[3], b[6]) + CMB_MULL(a[2], b[7]) + CMB_MULH(a[7], b[1]) + CMB_MULH(a[6], b[2]) + CMB_MULH(a[5], b[3]) + CMB_MULH(a[4], b[4]) + CMB_MULH(a[3], b[5]) + CMB_MULH(a[2], b[6]) + CMB_MULH(a[1], b[7]);
+	__uint128_t acc10 = CMB_MULL(a[7], b[3]) + CMB_MULL(a[6], b[4]) + CMB_MULL(a[5], b[5]) + CMB_MULL(a[4], b[6]) + CMB_MULL(a[3], b[7]) + CMB_MULH(a[7], b[2]) + CMB_MULH(a[6], b[3]) + CMB_MULH(a[5], b[4]) + CMB_MULH(a[4], b[5]) + CMB_MULH(a[3], b[6]) + CMB_MULH(a[2], b[7]);
+	__uint128_t acc11 = CMB_MULL(a[7], b[4]) + CMB_MULL(a[6], b[5]) + CMB_MULL(a[5], b[6]) + CMB_MULL(a[4], b[7]) + CMB_MULH(a[7], b[3]) + CMB_MULH(a[6], b[4]) + CMB_MULH(a[5], b[5]) + CMB_MULH(a[4], b[6]) + CMB_MULH(a[3], b[7]);
+	__uint128_t acc12 = CMB_MULL(a[7], b[5]) + CMB_MULL(a[6], b[6]) + CMB_MULL(a[5], b[7]) + CMB_MULH(a[7], b[4]) + CMB_MULH(a[6], b[5]) + CMB_MULH(a[5], b[6]) + CMB_MULH(a[4], b[7]);
+	__uint128_t acc13 = CMB_MULL(a[7], b[6]) + CMB_MULL(a[6], b[7]) + CMB_MULH(a[7], b[5]) + CMB_MULH(a[6], b[6]) + CMB_MULH(a[5], b[7]);
+	__uint128_t acc14 = CMB_MULL(a[7], b[7]) + CMB_MULH(a[7], b[6]) + CMB_MULH(a[6], b[7]);
+	__uint128_t acc15 = CMB_MULH(a[7], b[7]);
+
+	// Final carry propagation
+	uint8_t carry = 0;
+	__uint128_t sum = 0;
+
+	out[0] = acc0;
+	out[1] = CMB_L64(acc1);
+
+	sum = CMB_H64(acc1) + CMB_L64(acc2) + carry;
+	out[2] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc2) + CMB_L64(acc3) + carry;
+	out[3] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc3) + CMB_L64(acc4) + carry;
+	out[4] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc4) + CMB_L64(acc5) + carry;
+	out[5] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc5) + CMB_L64(acc6) + carry;
+	out[6] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc6) + CMB_L64(acc7) + carry;
+	out[7] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc7) + CMB_L64(acc8) + carry;
+	out[8] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc8) + CMB_L64(acc9) + carry;
+	out[9] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc9) + CMB_L64(acc10) + carry;
+	out[10] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc10) + CMB_L64(acc11) + carry;
+	out[11] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc11) + CMB_L64(acc12) + carry;
+	out[12] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc12) + CMB_L64(acc13) + carry;
+	out[13] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc13) + CMB_L64(acc14) + carry;
+	out[14] = CMB_L64(sum);
+	carry = CMB_H64(sum);
+
+	sum = CMB_H64(acc14) + CMB_L64(acc15) + carry;
+	out[15] = CMB_L64(sum);
+}
--- comba_mul/tests_c/test.c
+++ comba_mul/tests_c/test.c
@@ -10,6 +10,18 @@
  void comba320x320(uint64_t *out, uint64_t *a, uint64_t *b);
  #define comba_mul comba320x320
  #include "comba320x320_test_vectors_sample.c"
+#elif defined(COMBA384X384)
+ void comba384x384(uint64_t *out, uint64_t *a, uint64_t *b);
+ #define comba_mul comba384x384
+ #include "comba384x384_test_vectors_sample.c"
+#elif defined(COMBA448X448)
+ void comba448x448(uint64_t *out, uint64_t *a, uint64_t *b);
+ #define comba_mul comba448x448
+ #include "comba448x448_test_vectors_sample.c"
+#elif defined(COMBA512X512)
+ void comba512x512(uint64_t *out, uint64_t *a, uint64_t *b);
+ #define comba_mul comba512x512
+ #include "comba512x512_test_vectors_sample.c"
 #endif
 
 int main() {
